896a69d9043397b7c530972ced904d91e478e23f
Kenny Nguyen <ken_nggg@outlook.com>
2020-09-18 06:21:08 +1200

    fixxed a few issues with webserver on web.config

--- my-app/src/components/Home.tsx
+++ my-app/src/components/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import "./Home.css";
 
 function Home() {
-  return <h1> Home Page</h1>;
+  return (
+    <div className="home-wrapper">
+      <Typography variant="h4" component="h4">
+        Welcome to HealthyOrg!
+      </Typography>
+      <Typography variant="h6" component="h6">
+        Please click on sign in on the top right hand side to get started!
+      </Typography>
+    </div>
+  );
 }
 
 export default Home;
